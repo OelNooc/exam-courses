89e818ef61607f8d90eb33452c99e25f23ceceed
Leonardo Catejo <leo.catejoa@gmail.com>
2022-09-24 12:04:03 -0300

    Finish ListFragment

--- app/src/main/java/cl/nooc/examencentrodeformacinfuturo/ui/ListFragment.kt
+++ app/src/main/java/cl/nooc/examencentrodeformacinfuturo/ui/ListFragment.kt
@@ -5,16 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.Navigation
 import cl.nooc.examencentrodeformacinfuturo.R
+import cl.nooc.examencentrodeformacinfuturo.adapter.CoursesAdapter
+import cl.nooc.examencentrodeformacinfuturo.databinding.FragmentListBinding
+import cl.nooc.examencentrodeformacinfuturo.model.Course
+import cl.nooc.examencentrodeformacinfuturo.viewmodel.CoursesViewModel
 
 class ListFragment : Fragment() {
+
+    lateinit var binding: FragmentListBinding
+    private val viewModel by viewModels<CoursesViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+
+        binding = FragmentListBinding.inflate(layoutInflater)
+
+        val adapter = CoursesAdapter()
+        adapter.setCustomOnClickListener(object: CoursesAdapter.CustomOnClickListener{
+            override fun onClickListener(course: Course) {
+                viewModel.getCourseDetail(course.id)
+                viewModel.id.value = course.id
+
+                Navigation.findNavController(requireView()).navigate(R.id.action_listFragment_to_detailFragment)
+            }
+        })
+
+        binding.rvCourses.adapter = adapter
+
+        viewModel.coursesList.observe(viewLifecycleOwner, Observer {
+            adapter.updateData(it)
+        })
+        return binding.root
     }
 
 }
