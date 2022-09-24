438fb3b3f97f88a5aec6e4d67a8b7520fc475109
Leonardo Catejo <leo.catejoa@gmail.com>
2022-09-24 12:27:36 -0300

    Finish ListFragment

--- app/src/main/java/cl/nooc/examencentrodeformacinfuturo/model/Course.kt
+++ app/src/main/java/cl/nooc/examencentrodeformacinfuturo/model/Course.kt
@@ -1,9 +1,11 @@
 package cl.nooc.examencentrodeformacinfuturo.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "courses")
 data class Course(
+    @PrimaryKey
     val id: String,
     val image: String,
     val previewDescription: String,
--- app/src/main/java/cl/nooc/examencentrodeformacinfuturo/model/CourseDetail.kt
+++ app/src/main/java/cl/nooc/examencentrodeformacinfuturo/model/CourseDetail.kt
@@ -1,10 +1,12 @@
 package cl.nooc.examencentrodeformacinfuturo.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "course_detail")
 data class CourseDetail(
     val description: String,
+    @PrimaryKey
     val id: String,
     val image: String,
     val minimumSkill: String,
