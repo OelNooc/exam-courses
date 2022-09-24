0b73bfe44dbcafd4ea0ec38492d562b8b290c604
Leonardo Catejo <leo.catejoa@gmail.com>
2022-09-24 15:46:10 -0300

    Finish testing

--- app/src/androidTest/java/cl/nooc/examencentrodeformacinfuturo/room/CourseDBTest.kt
+++ app/src/androidTest/java/cl/nooc/examencentrodeformacinfuturo/room/CourseDBTest.kt
@@ -6,6 +6,7 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import cl.nooc.examencentrodeformacinfuturo.dao.CourseDetailDao
+import cl.nooc.examencentrodeformacinfuturo.getOrAwaitValue
 import cl.nooc.examencentrodeformacinfuturo.model.CourseDetail
 import com.google.common.truth.Truth
 import org.junit.After
@@ -36,7 +37,7 @@ class CourseDBTest {
     }
 
     @Test
-    fun addCourse(){
+    fun addCourseTest(){
         var courseDetail = CourseDetail("This course will provide you with all of the essentials to become a successful frontend web developer. You will learn to master HTML, CSS and front end JavaScript, along with tools like Git, VSCode and front endframeworks like Vue",
         "100", "https://user-images.githubusercontent.com/22780253/120053848-84763280-bffa-11eb-9d4c-a8f923e55d75.jpg",
         "beginner", "bootcamp", true, "november 2021",
@@ -45,5 +46,17 @@ class CourseDBTest {
         dao.addDetails(courseDetail)
         var response = dao.search("100")
         Truth.assertThat(response).isEqualTo(courseDetail)
+    }
+
+    @Test
+    fun listCourseTest(){
+        var courseDetail = CourseDetail("This course will provide you with all of the essentials to become a successful frontend web developer. You will learn to master HTML, CSS and front end JavaScript, along with tools like Git, VSCode and front endframeworks like Vue",
+            "100", "https://user-images.githubusercontent.com/22780253/120053848-84763280-bffa-11eb-9d4c-a8f923e55d75.jpg",
+            "beginner", "bootcamp", true, "november 2021",
+            "Front End Web Development", "$800.000", 8)
+
+        dao.addDetails(courseDetail)
+        val list = dao.listDetails().getOrAwaitValue()
+        Truth.assertThat(list[0].id).isEqualTo("100")
     }
 }
