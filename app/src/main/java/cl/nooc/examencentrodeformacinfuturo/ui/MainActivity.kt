96d202559326731c91ef66b15d0d21314f4f0b53
Leonardo Catejo <leo.catejoa@gmail.com>
2022-09-24 12:03:41 -0300

    Finish MainActivity

--- app/src/main/java/cl/nooc/examencentrodeformacinfuturo/ui/MainActivity.kt
+++ app/src/main/java/cl/nooc/examencentrodeformacinfuturo/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package cl.nooc.examencentrodeformacinfuturo.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import cl.nooc.examencentrodeformacinfuturo.R
+import cl.nooc.examencentrodeformacinfuturo.databinding.ActivityMainBinding
+import cl.nooc.examencentrodeformacinfuturo.viewmodel.CoursesViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private val viewModel by viewModels<CoursesViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.getAllCourses()
     }
 }
